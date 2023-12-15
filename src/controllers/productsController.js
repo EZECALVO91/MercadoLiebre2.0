88203ef81076d98bb284cfcee31787cc28c0826c
EZECALVO91 <ezequielcalvo@live.com>
2023-12-14 23:52:25 -0300

    ultimas actualizaciones

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const {loadProducts, storeProducts} = require('../data/productsDataBase.json')
+
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
@@ -37,12 +39,33 @@ const controller = {
 	},
 	// Update - Method to update
 	update: (req, res) => {
+		const {id} = req.params;
+		const {name,price,discount,category,description,image} = req.body 
+        const nuevoArray = products.map ( product => {
+			if (product.id == id){
+				return {
+					id,
+					name: name.trim(),
+					price,
+					discount,
+					category,
+					description : description.trim(),
+					image : image ? image : product.image,
+				}
+			}
+			return product
+		})
+		const json = JSON.stringify(nuevoArray);
+		fs.writeFileSync(productsFilePath,json, "utf-8");
+		res.redirect (`/products/detail/${id}`)
 		
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		let productsModify = loadProducts().filter(product => product.id !== +req.params.id);
+		storeProducts(productsModify);
+		res.redirect('/products')
 	}
 };
 
---
+++ src/data/containerLogic.js
@@ -0,0 +1,20 @@
+const fs = require ('fs');
+const path = require ('path');
+
+const loadProducts = () => {
+    const productsFilePath = path.join (__dirname, './productsDataBase.json');
+    const products = JSON.parse(fs.readFileSync(productsFilePath,'utf-8'));
+    return products;
+}
+
+const storeProducts = (products) => {
+    const usersFilePath = path.join (__dirname, './productsDataBase.json');
+    fs.writeFileSync(productsFilePath, JSON.stringify(products), 'utf-8')
+}
+
+
+
+module.exports = {
+    loadProducts,
+    storeProducts
+}
